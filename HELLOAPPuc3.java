2d945a4ce66917a77558c285fff82d3b82c34d3d
suryacollab99 <sk5178@srmist.edu.in>
2026-03-25 09:01:42 +0530

    added test file

---
+++ HELLOAPPuc3.java
@@ -0,0 +1,14 @@
+public class HELLOAPPUC3 {
+    public static void main(String[] args) {
+       
+        String name = "World";
+
+     
+        if (args.length > 0) {
+            name = args[0]; 
+        }
+
+       
+        System.out.println("Hello, " + name + "!");
+    }
+}
